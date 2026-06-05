04d5f867017cd5926bc90bc47406bc77015ea69e
GuiSmithMhnet <guilherme.rodrigues@mhnet.com.br>
2026-06-05 09:26:51 -0300

    Valida duplicidade ao criar usuario

--- back/src/services/usuarios.service.js
+++ back/src/services/usuarios.service.js
@@ -1,11 +1,59 @@
 import db from "../config/database.js";
+import ConflictError from "../errors/conflict.error.js";
 
 const sanitizeUsuario = (usuario) => {
   const { senha, ...safeUsuario } = usuario;
   return safeUsuario;
 };
 
+const cpfJaCadastrado = async (cpf) => {
+  const usuario = await db.usuario.findUnique({
+    where: {
+      cpf,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(usuario);
+};
+
+const emailJaCadastrado = async (email) => {
+  const usuario = await db.usuario.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(usuario);
+};
+
 const criarUsuario = async (usuarioDTO) => {
+  const [cpfCadastrado, emailCadastrado] = await Promise.all([
+    cpfJaCadastrado(usuarioDTO.cpf),
+    emailJaCadastrado(usuarioDTO.email),
+  ]);
+
+  const fields = [];
+
+  if (cpfCadastrado) {
+    fields.push("cpf");
+  }
+
+  if (emailCadastrado) {
+    fields.push("email");
+  }
+
+  if (fields.length > 0) {
+    throw new ConflictError("Usuário já cadastrado", {
+      fields,
+    });
+  }
+
   const usuario = await db.usuario.create({
     data: usuarioDTO,
   });
